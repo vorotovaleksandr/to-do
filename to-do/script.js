8fda10d18bbe50d0b68b7f3963e417bf4b3e69d1
vorotovaleksandr <vorotovaleksandr.exceedteam@gmail.com>
2019-03-27 12:26:20 +0300

    27/03/19

--- to-do/script.js
+++ to-do/script.js
@@ -1,6 +1,11 @@
-$(function (){ 
+var bgcolorlist = ['#ee6a68', '#f279a2', '#5eb3f6', '#67d7e5', '#ffe083' ];
+$(function(){
     $('#Add').click(function(){
-       var text = $('#text').val(); 
-       $('#min').text(text);
+        var text = $('#text').val();
+            $('h1').addClass('toDo').css({
+                background: bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
+            });
+            $('h1').text(text)
+            return;
+        }); 
     });
-});
