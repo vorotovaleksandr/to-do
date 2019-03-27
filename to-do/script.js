fd10399e7b575af7bcb81e7915f7b024fd373d7f
vorotovaleksandr <vorotovaleksandr.exceedteam@gmail.com>
2019-03-27 14:51:09 +0300

    27.03

--- to-do/script.js
+++ to-do/script.js
@@ -1,11 +1,31 @@
 var bgcolorlist = ['#ee6a68', '#f279a2', '#5eb3f6', '#67d7e5', '#ffe083' ];
+var cb = $('<input type = "checkbox"/>')
 $(function(){
-    $('#Add').click(function(){
+
+   
+       $('#Add').click(function(){
         var text = $('#text').val();
-            $('h1').addClass('toDo').css({
-                background: bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
+        $('#cb').addClass('cbox');
+        $('#min').addClass('newBlock');
+        $('p').css({
+            background: bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
             });
-            $('h1').text(text)
-            return;
-        }); 
+                 $('#cb').append(cb)
+                 $('#min').append(text)
+             return;
+        }) 
     });
+}
+    else{
+            $('#Add').click(function(){
+            var text = $('#text').val();
+            $('#cb').addClass('cbox');
+            $('#min').addClass('newBlock');
+            $('#red').click(function(){
+            $('P').addClass ('#red')
+            $('#cb').append(cb)
+            $('#min').append(text)
+                 return;
+            }); 
+        });
+    }
